6c00391de92fc83b39618cf8ea10000a8bfbde22
Severin Ibarluzea <seve@seve.blog>
2024-06-22 17:03:49 -0700

    wip

---
+++ lib/convert-easyeda-json-to-tscircuit-soup-json.ts
@@ -0,0 +1,8 @@
+import type { EasyEdaJson } from "./schemas/easy-eda-json-schema"
+import type { AnySoupElement } from "@tscircuit/soup"
+
+export const convertEasyEdaJsonToTscircuitSoupJson = (
+  easyEdaJson: EasyEdaJson
+): AnySoupElement[] => {
+  // TODO
+}
