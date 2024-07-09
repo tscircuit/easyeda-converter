dd5c028389e56a6f3a74598862728031663be48f
seveibar <seveibar@gmail.com>
2024-07-08 22:49:51 -0700

    repro of issue with ts gen

--- tests/convert-to-ts/C88224-to-ts.test.ts
+++ tests/convert-to-ts/C88224-to-ts.test.ts
@@ -13,5 +13,16 @@ it("should convert c88224 into typescript file", async () => {
     easyeda,
     soup,
   })
+
   console.log(result)
+
+  // This basically means a number wasn't converted properly- EasyEDA uses "mil"
+  // or "tenth-mil" for it's internal units, when we first parse them we add
+  // the "mil" suffix so that everything has a unit- but for some reason in some
+  // places we're not calling the mm(...) function from @tscircuit/mm to properly
+  // convert the number to mm OR we're taking the string "XXXmil" and just adding
+  // "mm" to it.... In tscircuit putting "XXXmil" OR "XXXmm" is acceptable but
+  // you can't do "XXXmilmm".
+  expect(result).not.toContain("milmm")
+  expect(result).not.toContain("NaNmm")
 })
