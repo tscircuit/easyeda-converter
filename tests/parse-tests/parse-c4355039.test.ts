edade45afee50750f7014bb8119aae6365f38377
Andrii Balitskyi <10balian10@gmail.com>
2024-07-10 16:55:51 +0200

    Remove console log

--- tests/parse-tests/parse-c4355039.test.ts
+++ tests/parse-tests/parse-c4355039.test.ts
@@ -4,7 +4,6 @@ import { EasyEdaJsonSchema } from "lib/schemas/easy-eda-json-schema"
 
 it("should parse easyeda json for a c4355039", async () => {
   const result = EasyEdaJsonSchema.parse(C4355039EasyEdaJson)
-  console.log("🚀 ~ result:", result)
 
   expect(result.uuid).toBe("6cbdfe3b239e4e5cb99069e0042dab7d")
 })
