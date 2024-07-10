36f377762f417d8edef498efdd4792efcf2812de
Andrii Balitskyi <10balian10@gmail.com>
2024-07-10 17:02:52 +0200

    Add soup conversion test

---
+++ tests/convert-to-soup-tests/c4355039-to-soup.test.ts
@@ -0,0 +1,14 @@
+import { it } from "bun:test"
+import { logSoup } from "@tscircuit/log-soup"
+import C4355039EasyEdaJson from "../assets/C4355039.raweasy.json"
+import { EasyEdaJsonSchema } from "lib/schemas/easy-eda-json-schema"
+import { convertEasyEdaJsonToTscircuitSoupJson } from "lib/convert-easyeda-json-to-tscircuit-soup-json"
+import type { AnySoupElement } from "@tscircuit/soup"
+
+it("should parse easyeda json for c4355039 and convert to tscircuit soup", async () => {
+  const parsedJson = EasyEdaJsonSchema.parse(C4355039EasyEdaJson)
+  console.log("🚀 ~ parsedJson:", parsedJson)
+  const soupElements = convertEasyEdaJsonToTscircuitSoupJson(parsedJson)
+
+  await logSoup("easyeda c4355039 to soup", soupElements as AnySoupElement[])
+})
