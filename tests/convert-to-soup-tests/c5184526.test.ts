b7dce6636f1b7c6892757e7032a7eb0d61dc2f49
seveibar <seveibar@gmail.com>
2024-10-12 16:55:01 -0400

    test cleanup

--- tests/convert-to-soup-tests/c5184526.test.ts
+++ tests/convert-to-soup-tests/c5184526.test.ts
@@ -5,11 +5,9 @@ import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 
 test("C5184526 should have two holes", () => {
   const bettereasy = EasyEdaJsonSchema.parse(c5184526)
-  Bun.write("./tmp.bettereasy.json", JSON.stringify(bettereasy, null, 2))
   const circuitJson = convertEasyEdaJsonToCircuitJson(bettereasy)
 
   expect(circuitJson.filter((e) => e.type === "pcb_hole").length).toBe(2)
-  Bun.write("./tmp.json", JSON.stringify(circuitJson, null, 2))
 
   expect(convertCircuitJsonToPcbSvg(circuitJson)).toMatchSvgSnapshot(
     import.meta.path,
