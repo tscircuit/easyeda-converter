4fba603519dabfdb31c7d740011494029c706846
Andrii Balitskyi <10balian10@gmail.com>
2024-10-19 20:41:51 +0200

    Add snapshot

--- tests/convert-to-ts/C2998002-to-ts.test.ts
+++ tests/convert-to-ts/C2998002-to-ts.test.ts
@@ -1,7 +1,10 @@
-import { it, expect } from "bun:test"
+import { test,it, expect } from "bun:test"
 import chipRawEasy from "../assets/C2998002.raweasy.json"
 import { convertBetterEasyToTsx } from "lib/convert-to-typescript-component"
 import { EasyEdaJsonSchema } from "lib/schemas/easy-eda-json-schema"
+import c2998002 from "tests/assets/C2998002.raweasy.json"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { convertEasyEdaJsonToCircuitJson } from "lib/convert-easyeda-json-to-tscircuit-soup-json"
 
 it("should convert C2998002 into typescript file", async () => {
   const betterEasy = EasyEdaJsonSchema.parse(chipRawEasy)
@@ -12,4 +15,13 @@ it("should convert C2998002 into typescript file", async () => {
   expect(result).not.toContain("milmm")
   expect(result).not.toContain("NaNmm")
   // Add more specific assertions here based on the component
+})
+
+test("C57759 should generate Circuit Json without errors", () => {
+  const betterEasy = EasyEdaJsonSchema.parse(c2998002)
+  const circuitJson = convertEasyEdaJsonToCircuitJson(betterEasy)
+
+  expect(convertCircuitJsonToPcbSvg(circuitJson)).toMatchSvgSnapshot(
+    import.meta.path,
+  )
 })
