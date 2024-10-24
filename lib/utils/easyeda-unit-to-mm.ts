539ba2d2890d5a5471a829fffe7de570c313b896
Andrii Balitskyi <84702959+andrii-balitskyi@users.noreply.github.com>
2024-10-24 13:22:18 +0200

    Fix stroke width calculation (#84)
    * Fix stroke width calculation
    
    * Update easyeda ref link, format
    
    * REmove console log
    
    * Rename easyEdaUnitToMm to mil10ToMm

---
+++ lib/utils/easyeda-unit-to-mm.ts
@@ -0,0 +1,9 @@
+/**
+ * EasyEDA takes 10 mil as a basic factor, when a stroke width is 1, we can take it as 1*10mil = 10mil, is 2, we can take it as 2*10mil = 20mil,
+ * Ref: https://docs.easyeda.com/en/DocumentFormat/3-EasyEDA-PCB-File-Format/#unit
+ *
+ * 1 mil = 0.001 inch
+ * 1 inch = 25.4 mm
+ * 1 mil = 25.4/1000 = 0.0254 mm
+ */
+export const mil10ToMm = (value: number): number => value * 10 * 0.0254
