0785b8a28bca3669bc2bd5ad0c059217d824b4da
Ayush Jhawar <ayushjhawar499@gmail.com>
2025-03-30 21:56:43 +0530

    add visual snapshot

--- tests/fetch-tests/c41430893.test.ts
+++ tests/fetch-tests/c41430893.test.ts
@@ -1,9 +1,22 @@
 import { it, expect } from "bun:test"
 import { fetchEasyEDAComponent } from "lib/websafe/fetch-easyeda-json"
+import { EasyEdaJsonSchema } from "lib/schemas/easy-eda-json-schema"
+import { convertEasyEdaJsonToCircuitJson } from "lib/convert-easyeda-json-to-tscircuit-soup-json"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 
 it("should fetch correct part for C41430893", async () => {
   const result = await fetchEasyEDAComponent("C41430893")
 
   // The fetched data should match C41430893
   expect(result.dataStr?.head?.c_para?.["Supplier Part"]).toBe("C41430893")
+})
+
+it("C41430893 should generate Circuit Json without errors", async () => {
+  const rawEasy = await fetchEasyEDAComponent("C41430893")
+  const betterEasy = EasyEdaJsonSchema.parse(rawEasy)
+  const circuitJson = convertEasyEdaJsonToCircuitJson(betterEasy)
+
+  expect(convertCircuitJsonToPcbSvg(circuitJson)).toMatchSvgSnapshot(
+    import.meta.path,
+  )
 })
