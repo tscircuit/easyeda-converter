e0bcc4a55e9848b91ccbb6df93609274f0c61057
Anshgrover23 <ag5989670@gmail.com>
2025-05-12 11:06:56 +0530

    add C5248081 part

--- tests/convert-to-ts/C5248081-to-ts.test.ts
+++ tests/convert-to-ts/C5248081-to-ts.test.ts
@@ -2,6 +2,8 @@ import { it, expect } from "bun:test"
 import chipRawEasy from "../assets/C5248081.raweasy.json"
 import { convertBetterEasyToTsx } from "lib/websafe/convert-to-typescript-component"
 import { EasyEdaJsonSchema } from "lib/schemas/easy-eda-json-schema"
+import { convertEasyEdaJsonToCircuitJson } from "lib/convert-easyeda-json-to-tscircuit-soup-json"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 
 it("should convert C5248081 into typescript file", async () => {
   const betterEasy = EasyEdaJsonSchema.parse(chipRawEasy)
@@ -52,4 +54,13 @@ it("should convert C5248081 into typescript file", async () => {
       )
     }"
   `)
+})
+
+it("C5248081 should generate Circuit Json without errors", () => {
+  const betterEasy = EasyEdaJsonSchema.parse(chipRawEasy)
+  const circuitJson = convertEasyEdaJsonToCircuitJson(betterEasy)
+
+  expect(convertCircuitJsonToPcbSvg(circuitJson)).toMatchSvgSnapshot(
+    import.meta.path,
+  )
 })
