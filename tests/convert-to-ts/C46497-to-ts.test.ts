61836f3c2df7b40a4e0fd5c50313c389d56264a0
Severin Ibarluzea <seve700@gmail.com>
2025-06-29 20:44:49 -0700

    feat: add via conversion for tsx

---
+++ tests/convert-to-ts/C46497-to-ts.test.ts
@@ -0,0 +1,15 @@
+import { it, expect } from "bun:test"
+import chipRawEasy from "../assets/C46497.raweasy.json"
+import { convertBetterEasyToTsx } from "lib/websafe/convert-to-typescript-component"
+import { EasyEdaJsonSchema } from "lib/schemas/easy-eda-json-schema"
+
+it("should convert C46497 with vias into typescript file", async () => {
+  const betterEasy = EasyEdaJsonSchema.parse(chipRawEasy)
+  const result = await convertBetterEasyToTsx({
+    betterEasy,
+  })
+
+  expect(result).not.toContain("milmm")
+  expect(result).not.toContain("NaNmm")
+  expect(result).toContain("<via")
+})
