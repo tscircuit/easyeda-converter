1153a04dfe07da99c31b4d8bd17dd493d5235895
Severin Ibarluzea <seve700@gmail.com>
2025-08-08 10:27:58 -0700

    fix: refine colors schema typing

--- lib/schemas/easy-eda-json-schema.ts
+++ lib/schemas/easy-eda-json-schema.ts
@@ -83,7 +83,7 @@ export const DataStrSchema = z.object({
   canvas: z.string(),
   shape: z.array(SingleLetterShapeSchema),
   BBox: BBoxSchema,
-  colors: z.array(z.unknown()).or(z.record(z.unknown())),
+  colors: z.union([z.array(z.string()), z.record(z.string())]),
 })
 
 export const PackageDetailDataStrSchema = z.object({
