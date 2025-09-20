a3c806925f0b944e701b3ae3cd8afb79264693a5
Quanta-Naut <123290216+Quanta-Naut@users.noreply.github.com>
2025-09-20 20:15:41 +0530

    Fix [C388629] Failed to import from JLCPCB (#124)

--- lib/websafe/fetch-easyeda-json.ts
+++ lib/websafe/fetch-easyeda-json.ts
@@ -66,6 +66,13 @@ export async function fetchEasyEDAComponent(
   }
 
   const componentResult = await componentResponse.json()
+
+    if (componentResult.result.szlcsc && typeof componentResult.result.szlcsc.id === "string") {
+    componentResult.result.szlcsc.id = Number(componentResult.result.szlcsc.id)
+  }
+
+  console.log("Transformed szlcsc.id:", componentResult.result.szlcsc?.id);
+
   return componentResult.result
 }
 
