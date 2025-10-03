8fe6ee1278fd7cdee6ae7974218dae5b77710009
Quanta-Naut <123290216+Quanta-Naut@users.noreply.github.com>
2025-10-03 20:29:53 +0530

    Fixed the solder mask shape

--- lib/convert-easyeda-json-to-tscircuit-soup-json.ts
+++ lib/convert-easyeda-json-to-tscircuit-soup-json.ts
@@ -315,13 +315,11 @@ export const convertEasyEdaJsonToCircuitJson = (
             : smallestInnerDimension
 
         additionalPlatedHoleProps = {
-          shape: "pill_hole_with_rect_pad",
-          hole_shape: "pill",
-          pad_shape: "rect",
+          shape: "pill",
+          outer_width: mil2mm(pad.width),
+          outer_height: mil2mm(pad.height),
           hole_width: innerWidth,
           hole_height: innerHeight,
-          rect_pad_width: mil2mm(pad.width),
-          rect_pad_height: mil2mm(pad.height),
         }
       } else if (pad.shape === "RECT") {
         const padWidth = mil2mm(pad.width)
