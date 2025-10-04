976ae54851cbd18569544bcdec06467d1b80bdaa
Quanta-Naut <123290216+Quanta-Naut@users.noreply.github.com>
2025-10-04 18:57:54 +0530

    Fixed CI test

--- tests/convert-to-ts/C165948-to-ts.test.ts
+++ tests/convert-to-ts/C165948-to-ts.test.ts
@@ -1,4 +1,3 @@
-
 import { it, expect } from "bun:test"
 import chipRawEasy from "../assets/C165948.raweasy.json"
 import { convertBetterEasyToTsx } from "lib/websafe/convert-to-typescript-component"
@@ -14,7 +13,7 @@ it("should convert C165948 into typescript file", async () => {
   expect(result).not.toContain("NaNmm")
 
   // Add more specific assertions here based on the component
-  
+
   expect(result).toMatchInlineSnapshot(`
     "import type { ChipProps } from "@tscircuit/props"
 
