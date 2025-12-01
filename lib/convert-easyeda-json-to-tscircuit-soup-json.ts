595498a32b548ae0953f80207907432ee2da15ca
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-12-01 21:33:46 +0530

    Handle circuit-json type errors (#330)
    * Handle circuit-json type errors
    
    * update test

--- lib/convert-easyeda-json-to-tscircuit-soup-json.ts
+++ lib/convert-easyeda-json-to-tscircuit-soup-json.ts
@@ -558,7 +558,9 @@ export const convertEasyEdaJsonToCircuitJson = (
         if (e.type === "pcb_cutout") {
           if (e.shape === "polygon") {
             e.points = e.points.map((p) => applyToPoint(matrix, p))
-          } else {
+          } else if (e.shape === "path") {
+            e.route = e.route.map((p) => applyToPoint(matrix, p))
+          } else if (e.shape === "circle" || e.shape === "rect") {
             e.center = applyToPoint(matrix, e.center)
           }
         } else if (e.type === "pcb_smtpad" && e.shape === "polygon") {
