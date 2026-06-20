9faf44f1e0205cf999ddad43aa2eb46c2e1aea5e
Rushabh Patil <mail@rushabh.dev>
2026-06-20 21:43:31 +0530

    test: update expected TS conversion output for connector footprint offsets

--- tests/convert-to-ts/C18185602-to-ts.test.ts
+++ tests/convert-to-ts/C18185602-to-ts.test.ts
@@ -42,32 +42,32 @@ it("should convert C18185602 into typescript file", async () => {
     }}
           manufacturerPartNumber="PJ_320A_4P_DIP"
           footprint={<footprint>
-            <hole pcbX="2.149983mm" pcbY="0mm" diameter="1.1999976mm" />
-    <hole pcbX="-4.850003mm" pcbY="0mm" diameter="1.1999976mm" />
-    <platedhole  portHints={["pin1"]} pcbX="3.749929mm" pcbY="2.29997mm" holeWidth="0.5999988mm" holeHeight="1.499997mm" outerWidth="1.1999976mm" outerHeight="2.0999958mm" pcbRotation="90deg" shape="pill" />
-    <platedhole  portHints={["pin2"]} pcbX="-0.250063mm" pcbY="2.29997mm" holeWidth="0.5999988mm" holeHeight="1.499997mm" outerWidth="1.1999976mm" outerHeight="2.0999958mm" pcbRotation="90deg" shape="pill" />
-    <platedhole  portHints={["pin3"]} pcbX="-3.250057mm" pcbY="2.29997mm" holeWidth="0.5999988mm" holeHeight="1.499997mm" outerWidth="1.1999976mm" outerHeight="2.0999958mm" pcbRotation="90deg" shape="pill" />
-    <platedhole  portHints={["pin4"]} pcbX="4.850003mm" pcbY="-2.29997mm" holeWidth="0.5999988mm" holeHeight="1.499997mm" outerWidth="1.1999976mm" outerHeight="2.0999958mm" pcbRotation="90deg" shape="pill" />
-    <silkscreenpath route={[{"x":-6.449974399999974,"y":-2.4595836000000872},{"x":-6.4499490000000606,"y":3.0558485999999903}]} />
-    <silkscreenpath route={[{"x":-8.433943,"y":-2.5000204000000394},{"x":-6.449974399999974,"y":-2.5000204000000394}]} />
-    <silkscreenpath route={[{"x":-8.433943,"y":2.499969599999986},{"x":-6.433947000000103,"y":2.499969599999986}]} />
-    <silkscreenpath route={[{"x":-8.433943,"y":2.499969599999986},{"x":-8.433943,"y":-2.5000204000000394}]} />
-    <silkscreenpath route={[{"x":5.666028800000049,"y":-3.175025400000095},{"x":-6.449974399999974,"y":-3.175025400000095},{"x":-6.449974399999974,"y":-2.300096999999937}]} />
-    <silkscreenpath route={[{"x":5.666028800000049,"y":-3.0925516000000925},{"x":5.666028800000049,"y":-3.175025400000095}]} />
-    <silkscreenpath route={[{"x":5.666028800000049,"y":3.0558485999999903},{"x":5.666028800000049,"y":-1.5074899999999616}]} />
-    <silkscreenpath route={[{"x":4.658004399999868,"y":3.0558485999999903},{"x":5.666028800000049,"y":3.0558485999999903}]} />
-    <silkscreenpath route={[{"x":0.6580123999999614,"y":3.0558485999999903},{"x":3.074034999999867,"y":3.0558485999999903}]} />
-    <silkscreenpath route={[{"x":-2.34198159999994,"y":3.0558485999999903},{"x":-0.9259569999999258,"y":3.0558485999999903}]} />
-    <silkscreenpath route={[{"x":-6.433947000000103,"y":3.0558485999999903},{"x":-3.925950999999941,"y":3.0558485999999903}]} />
-    <silkscreenpath route={[{"x":5.666028800000049,"y":-2.7312365999999884},{"x":5.666028800000049,"y":-2.848101999999926}]} />
-    <silkscreentext text="{NAME}" pcbX="-1.381379mm" pcbY="4.06197mm" anchorAlignment="center" fontSize="1mm" />
-    <courtyardoutline outline={[{"x":-8.717979000000014,"y":3.311969999999974},{"x":5.955221000000051,"y":3.311969999999974},{"x":5.955221000000051,"y":-3.4110299999999825},{"x":-8.717979000000014,"y":-3.4110299999999825},{"x":-8.717979000000014,"y":3.311969999999974}]} />
+            <hole pcbX="2.4249824mm" pcbY="0mm" diameter="1.1999976mm" />
+    <hole pcbX="-4.5750036mm" pcbY="0mm" diameter="1.1999976mm" />
+    <platedhole  portHints={["pin1"]} pcbX="4.0249284mm" pcbY="2.29997mm" holeWidth="0.5999988mm" holeHeight="1.499997mm" outerWidth="1.1999976mm" outerHeight="2.0999958mm" pcbRotation="90deg" shape="pill" />
+    <platedhole  portHints={["pin2"]} pcbX="0.0249364mm" pcbY="2.29997mm" holeWidth="0.5999988mm" holeHeight="1.499997mm" outerWidth="1.1999976mm" outerHeight="2.0999958mm" pcbRotation="90deg" shape="pill" />
+    <platedhole  portHints={["pin3"]} pcbX="-2.9750576mm" pcbY="2.29997mm" holeWidth="0.5999988mm" holeHeight="1.499997mm" outerWidth="1.1999976mm" outerHeight="2.0999958mm" pcbRotation="90deg" shape="pill" />
+    <platedhole  portHints={["pin4"]} pcbX="5.1250024mm" pcbY="-2.29997mm" holeWidth="0.5999988mm" holeHeight="1.499997mm" outerWidth="1.1999976mm" outerHeight="2.0999958mm" pcbRotation="90deg" shape="pill" />
+    <silkscreenpath route={[{"x":-6.174975000000018,"y":-2.4595836000000872},{"x":-6.1749496000001045,"y":3.0558485999999903}]} />
+    <silkscreenpath route={[{"x":-8.158943600000043,"y":-2.5000204000000394},{"x":-6.174975000000018,"y":-2.5000204000000394}]} />
+    <silkscreenpath route={[{"x":-8.158943600000043,"y":2.499969599999986},{"x":-6.158947600000147,"y":2.499969599999986}]} />
+    <silkscreenpath route={[{"x":-8.158943600000043,"y":2.499969599999986},{"x":-8.158943600000043,"y":-2.5000204000000394}]} />
+    <silkscreenpath route={[{"x":5.941028200000005,"y":-3.175025400000095},{"x":-6.174975000000018,"y":-3.175025400000095},{"x":-6.174975000000018,"y":-2.300096999999937}]} />
+    <silkscreenpath route={[{"x":5.941028200000005,"y":-3.0925516000000925},{"x":5.941028200000005,"y":-3.175025400000095}]} />
+    <silkscreenpath route={[{"x":5.941028200000005,"y":3.0558485999999903},{"x":5.941028200000005,"y":-1.5074899999999616}]} />
+    <silkscreenpath route={[{"x":4.933003799999824,"y":3.0558485999999903},{"x":5.941028200000005,"y":3.0558485999999903}]} />
+    <silkscreenpath route={[{"x":0.9330117999999175,"y":3.0558485999999903},{"x":3.349034399999823,"y":3.0558485999999903}]} />
+    <silkscreenpath route={[{"x":-2.066982199999984,"y":3.0558485999999903},{"x":-0.6509575999999697,"y":3.0558485999999903}]} />
+    <silkscreenpath route={[{"x":-6.158947600000147,"y":3.0558485999999903},{"x":-3.650951599999985,"y":3.0558485999999903}]} />
+    <silkscreenpath route={[{"x":5.941028200000005,"y":-2.7312365999999884},{"x":5.941028200000005,"y":-2.848101999999926}]} />
+    <silkscreentext text="{NAME}" pcbX="-1.1063796mm" pcbY="4.06197mm" anchorAlignment="center" fontSize="1mm" />
+    <courtyardoutline outline={[{"x":-8.442979600000058,"y":3.311969999999974},{"x":6.230220400000007,"y":3.311969999999974},{"x":6.230220400000007,"y":-3.4110299999999825},{"x":-8.442979600000058,"y":-3.4110299999999825},{"x":-8.442979600000058,"y":3.311969999999974}]} />
           </footprint>}
           cadModel={{
             objUrl: "https://modelcdn.tscircuit.com/easyeda_models/assets/C18185602.obj?uuid=f90b68db56444592a325e6a33e48bfdb",
             stepUrl: "https://modelcdn.tscircuit.com/easyeda_models/assets/C18185602.step?uuid=f90b68db56444592a325e6a33e48bfdb",
             pcbRotationOffset: 0,
-            modelOriginPosition: { x: 1.383957099999975, y: -0.0050000000000001155, z: -2.5000069999999996 },
+            modelOriginPosition: { x: 1.108957700000019, y: -0.0050000000000001155, z: -2.5000069999999996 },
           }}
           {...props}
         />
--- tests/convert-to-ts/C19795120-to-ts.test.ts
+++ tests/convert-to-ts/C19795120-to-ts.test.ts
@@ -42,22 +42,22 @@ it("should convert C19795120 into typescript file", async () => {
     }}
           manufacturerPartNumber="A_470533000"
           footprint={<footprint>
-            <hole pcbX="-1.27mm" pcbY="-1.4850046mm" diameter="1.2499848mm" />
-    <platedhole  portHints={["pin1"]} pcbX="3.81mm" pcbY="0.6750114mm" outerDiameter="1.7199864mm" holeDiameter="1.0200132mm" shape="circle" />
-    <platedhole  portHints={["pin2"]} pcbX="1.27mm" pcbY="0.6750114mm" outerDiameter="1.7199864mm" holeDiameter="1.0200132mm" shape="circle" />
-    <platedhole  portHints={["pin3"]} pcbX="-1.27mm" pcbY="0.6750114mm" outerDiameter="1.7199864mm" holeDiameter="1.0200132mm" shape="circle" />
-    <platedhole  portHints={["pin4"]} pcbX="-3.81mm" pcbY="0.6750114mm" outerDiameter="1.7199864mm" holeDiameter="1.0200132mm" shape="circle" />
-    <silkscreenpath route={[{"x":-5.080000000000041,"y":2.2696488000000272},{"x":5.0799999999998136,"y":2.2696488000000272}]} />
-    <silkscreenpath route={[{"x":-1.9940778000001274,"y":-1.864988599999947},{"x":-5.080000000000041,"y":-1.864988599999947}]} />
-    <silkscreenpath route={[{"x":-5.080000000000041,"y":-1.864988599999947},{"x":-5.080000000000041,"y":3.9770114000000376},{"x":5.0799999999998136,"y":3.9770114000000376},{"x":5.0799999999998136,"y":-1.864988599999947},{"x":-0.5459222000001773,"y":-1.864988599999947}]} />
-    <silkscreentext text="{NAME}" pcbX="-0.0127mm" pcbY="4.9770114mm" anchorAlignment="center" fontSize="1mm" />
-    <courtyardoutline outline={[{"x":-5.380800000000022,"y":4.227011400000038},{"x":5.3554000000000315,"y":4.227011400000038},{"x":5.3554000000000315,"y":-2.622988599999985},{"x":-5.380800000000022,"y":-2.622988599999985},{"x":-5.380800000000022,"y":4.227011400000038}]} />
+            <hole pcbX="-1.27mm" pcbY="-1.1975084mm" diameter="1.2499848mm" />
+    <platedhole  portHints={["pin1"]} pcbX="3.81mm" pcbY="0.9625076mm" outerDiameter="1.7199864mm" holeDiameter="1.0200132mm" shape="circle" />
+    <platedhole  portHints={["pin2"]} pcbX="1.27mm" pcbY="0.9625076mm" outerDiameter="1.7199864mm" holeDiameter="1.0200132mm" shape="circle" />
+    <platedhole  portHints={["pin3"]} pcbX="-1.27mm" pcbY="0.9625076mm" outerDiameter="1.7199864mm" holeDiameter="1.0200132mm" shape="circle" />
+    <platedhole  portHints={["pin4"]} pcbX="-3.81mm" pcbY="0.9625076mm" outerDiameter="1.7199864mm" holeDiameter="1.0200132mm" shape="circle" />
+    <silkscreenpath route={[{"x":-5.080000000000041,"y":2.5571449999999913},{"x":5.0799999999998136,"y":2.5571449999999913}]} />
+    <silkscreenpath route={[{"x":-1.9940778000001274,"y":-1.5774923999999828},{"x":-5.080000000000041,"y":-1.5774923999999828}]} />
+    <silkscreenpath route={[{"x":-5.080000000000041,"y":-1.5774923999999828},{"x":-5.080000000000041,"y":4.264507600000002},{"x":5.0799999999998136,"y":4.264507600000002},{"x":5.0799999999998136,"y":-1.5774923999999828},{"x":-0.5459222000001773,"y":-1.5774923999999828}]} />
+    <silkscreentext text="{NAME}" pcbX="-0.0127mm" pcbY="5.2645076mm" anchorAlignment="center" fontSize="1mm" />
+    <courtyardoutline outline={[{"x":-5.380800000000022,"y":4.514507600000002},{"x":5.3554000000000315,"y":4.514507600000002},{"x":5.3554000000000315,"y":-2.335492400000021},{"x":-5.380800000000022,"y":-2.335492400000021},{"x":-5.380800000000022,"y":4.514507600000002}]} />
           </footprint>}
           cadModel={{
             objUrl: "https://modelcdn.tscircuit.com/easyeda_models/assets/C19795120.obj?uuid=9a93067adc7a4c82860f56e3024924ad",
             stepUrl: "https://modelcdn.tscircuit.com/easyeda_models/assets/C19795120.step?uuid=9a93067adc7a4c82860f56e3024924ad",
             pcbRotationOffset: 0,
-            modelOriginPosition: { x: 0, y: -0.6350208000000792, z: -3.295008 },
+            modelOriginPosition: { x: 0, y: -0.9225170000000433, z: -3.295008 },
           }}
           {...props}
         />
