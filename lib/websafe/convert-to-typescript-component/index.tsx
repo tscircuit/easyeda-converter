12ebf4ec2f3faa8c139bbea56b518dd47a2af41d
hrithik18k <hrithikvasanthram@gmail.com>
2026-09-12 01:38:35 +0530

    Revert "refactor: separate polarized capacitor detection"
    This reverts commit 27af4a927ee31de24dd084f74d76ccb8d06b1e55.

--- lib/websafe/convert-to-typescript-component/index.tsx
+++ lib/websafe/convert-to-typescript-component/index.tsx
@@ -15,6 +15,7 @@ import { generateTypescriptComponent } from "./generate-typescript-component"
 import type { GeneratedComponentType } from "./generate-typescript-component"
 import {
   isCapacitorComponent,
+  isPolarizedCapacitorComponent,
   normalizeCapacitanceValue,
 } from "./is-capacitor-component"
 import { isCrystalComponent } from "./is-crystal-component"
@@ -23,7 +24,6 @@ import { isDipSwitchCategoryComponent } from "./is-dip-switch-category-component
 import { isInductorComponent } from "./is-inductor-component"
 import { isLedCategoryComponent } from "./is-led-category-component"
 import { isMicroUsbConnectorComponent } from "./is-micro-usb-connector-component"
-import { isPolarizedCapacitorComponent } from "./is-polarized-capacitor-component"
 import { isPushbuttonCategoryComponent } from "./is-pushbutton-category-component"
 import {
   isResistorComponent,
